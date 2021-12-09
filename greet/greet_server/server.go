a7619c2e4151631f58efb53fc2a8cf34ac3a1495
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-09 11:00:25 +0100

    create server.go

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,1 +1,25 @@
-package greet_server
+package main
+
+import (
+	"fmt"
+	"github.com/Ad3bay0c/gRPC/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {}
+func main() {
+	fmt.Println("Hello World!")
+
+	listen, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err.Error())
+	}
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("Failed to serve: %v", err.Error())
+	}
+}
