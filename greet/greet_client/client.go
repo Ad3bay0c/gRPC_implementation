e54ff154115ffaf782bafdd02b84ad1eb208fcb8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-09 11:17:38 +0100

    Implemented the Client side

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,1 +1,18 @@
-package greet_client
+package main
+
+import (
+	"github.com/Ad3bay0c/gRPC/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldn't connect: %v", err.Error())
+	}
+	defer conn.Close()
+
+	c := greetpb.NewGreetServiceClient(conn)
+	log.Printf("Created Client: %f\n", c)
+}
