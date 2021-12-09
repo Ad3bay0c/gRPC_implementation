499e8f755f685902ebca6e35608350d758411120
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-09 11:07:52 +0100

    boiler plate code set up

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Ad3bay0c/gRPC/greet/greetpb"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
 )
 
 type server struct {}
 func main() {
-	fmt.Println("Hello World!")
 
-	listen, err := net.Listen("tcp", "0.0.0.0:50051")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "0.0.0.0:50051"
+	}
+
+	listen, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err.Error())
 	}
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
 
+	log.Printf("Server Started on port: %v\n", port)
 	if err := s.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve: %v", err.Error())
 	}
