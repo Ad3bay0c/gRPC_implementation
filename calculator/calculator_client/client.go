745846247740ce6289bf2b95ee7549f963596e13
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-10 13:31:08 +0100

    client rpc implemented

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -1,1 +1,31 @@
-package calculator_client
+package main
+
+import (
+	"context"
+	"github.com/Ad3bay0c/gRPC/calculator/calculatorpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldn't connect to server: %v", err.Error())
+	}
+	defer conn.Close()
+	c := calculatorpb.NewCalculatorServiceClient(conn)
+	doUnary(c)
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	req := &calculatorpb.SumRequest{
+        FirstNumber:  5,
+        SecondNumber: 10,
+    }
+    res, err := c.Sum(context.Background(), req)
+    if err != nil {
+        log.Fatalf("Error while calling Sum RPC: %v", err.Error())
+    }
+    log.Printf("Response from Sum: %v", res.SumResult)
+}
+
