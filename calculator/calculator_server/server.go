dfaceefc56f875050aab1a788b0070dda4491994
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-10 13:31:45 +0100

    calculaltor server side implementation

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -1,1 +1,29 @@
-package calculator_server
+package main
+
+import (
+	"context"
+	"github.com/Ad3bay0c/gRPC/calculator/calculatorpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+type server struct{}
+
+func (s *server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	return &calculatorpb.SumResponse{
+		SumResult: req.GetFirstNumber() + req.GetSecondNumber(),
+	}, nil
+}
+func main() {
+	listen, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err.Error())
+	}
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	log.Printf("Server Started\n")
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("Failed to Start Server: %v", err.Error())
+	}
+}
