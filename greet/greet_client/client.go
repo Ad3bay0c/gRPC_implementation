f473768b1f75adfef3d0cf02be984e7b4e9589a0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-10 13:32:26 +0100

    greetings client implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/Ad3bay0c/gRPC/greet/greetpb"
 	"google.golang.org/grpc"
 	"log"
@@ -14,5 +15,22 @@ func main() {
 	defer conn.Close()
 
 	c := greetpb.NewGreetServiceClient(conn)
-	log.Printf("Created Client: %f\n", c)
+
+	// Unary RPC Implementation
+	doUnary(c)
 }
+
+func doUnary(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "John",
+			LastName: "Doe",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err.Error())
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
+
