4956df9775cc5cafd0fb4636bb211118558d6637
Ad3bay0c <aclintonadebayo@gmail.com>
2021-12-10 17:21:33 +0100

    Server streaming implementation at greet_client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Ad3bay0c/gRPC/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -17,7 +18,10 @@ func main() {
 	c := greetpb.NewGreetServiceClient(conn)
 
 	// Unary RPC Implementation
-	doUnary(c)
+	//doUnary(c)
+
+	// Server Streaming RPC Implementation
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -34,3 +38,25 @@ func doUnary(c greetpb.GreetServiceClient) {
 	log.Printf("Response from Greet: %v", res.Result)
 }
 
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "John",
+			LastName: "Doe",
+		},
+	}
+	res, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("An error occurred: %v", err.Error())
+	}
+	for {
+		msg, err := res.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("An Error occurred: %v", err.Error())
+		}
+		log.Printf("Response : %v", msg.GetResult())
+	}
+}
